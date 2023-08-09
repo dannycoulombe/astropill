82a3333d22c2dd6df0ab6608cb4187d5a54ead7c
Danny Coulombe <contact@dannycoulombe.com>
2023-08-09 15:22:49 -0500

    nametable + music working

--- src/main.s
+++ src/main.s
@@ -1,19 +1,29 @@
 ; Load NES headers
 .segment "HEADER"
   .byte $4E, $45, $53, $1A            ; iNES header identifier
-  .byte 2                             ; 2x 16KB PRG-ROM Banks
-  .byte 1                             ; 1x  8KB CHR-ROM
-  .byte $00                           ; mapper 0 (NROM)
-  .byte $00                           ; System: NES
+  .byte $02                           ; amount of PRG ROM in 16K units
+  .byte $01                           ; amount of CHR ROM in 8K units
+  .byte $00                           ; mapper and mirroing
+  .byte $00, $00, $00, $00
+  .byte $00, $00, $00, $00, $00
 
 .segment "ZEROPAGE"
+  VAR:  .RES 1                              ; reserves 1 byte of memory for a variable named VAR
+
 .segment "STARTUP"
+
+.segment "RAM"
+
+.segment "OAM"
+  oam: .res 256        ; sprite OAM data to be uploaded by DMA
+
 .segment "CODE"
 
   ; Define variables
   .include "variables.inc"
 
   ; Prepare utils
+  .include "utils/apu.s"
   .include "utils/ppu.s"
 
   ; Prepare states
@@ -22,37 +32,37 @@
 ;  .include "states/sprite.s"
   .include "states/controller.s"
 
-  .proc nmi
-;    JSR PPU::vBlankWait
-;    JSR Controller::init
-    RTI
-  .endproc
+  ; Load drivers
+  .include "drvs/famistudio.s"
 
   .proc reset
     SEI
-    CLD                             ; Clear Decimal Mode (2A03 does not support BCD)
 
     ; Turn off NMI and rendering
     JSR PPU::disableNMI
+    CLD                             ; Clear Decimal Mode (2A03 does not support BCD)
+    ldx #$FF
+    txs       ; initialize stack
 
     ; PPU warm up
     LDA PPUSTATUS
     JSR PPU::vBlankWait
-    JSR PPU::vBlankWait
 
-    ; Clear memory (do not scope the following since JSR stores its state in $100-$1FF)
+    ; Clear memory
     LDX #0
     ClearRAM:
-      STA $000,x
-      STA $100,x
-      STA $200,x
-      STA $300,x
-      STA $400,x
-      STA $500,x
-      STA $600,x
-      STA $700,x
+      STA $0000,x
+      STA $0100,x
+      STA $0200,x
+      STA $0300,x
+      STA $0400,x
+      STA $0500,x
+      STA $0600,x
+      STA $0700,x
       INX
       BNE ClearRAM
+
+    JSR PPU::vBlankWait
 
     ; Activate DMA (direct memory access) by setting low and high byte
     ActivateDMA:
@@ -60,20 +70,49 @@
       STA OAMADDR                     ; set the low byte (00) of the RAM address
       LDA #$02
       STA OAMDMA                      ; set the high byte (02) of the RAM address, start the transfer
+    NOP
 
-    JSR Background::init
-    JSR Palette::init
+    LDA PPUSTATUS
+    LDA #$3F
+    STA PPUADDR
+    LDA #$00
+    STA PPUADDR
+
+;    JSR Palette::init
 ;    JSR Sprite::init
 
-    CLI
-
     ;Turn on NMI and rendering
+    JSR Background::init
     JSR PPU::resetScrolling
     JSR PPU::enableNMI
+    JSR APU::playSong
+    CLI
+
+    @draw_done:
+      jsr famistudio_update ; TODO: Call in NMI.
+      LDX #0
+      LDY #0
+      @outer_loop_nop:
+      @inner_loop_nop:
+        NOP
+        INY
+        CPY #255
+        BNE @inner_loop_nop
+        INX
+        CPX #12
+        BNE @outer_loop_nop
+
+      JMP @draw_done ; Dont allow update if we already have an update pending.
 
     ; Loop indefinitely
     Forever:
       JMP Forever
+  .endproc
+
+  .proc nmi
+;    JSR PPU::vBlankWait
+;    JSR Controller::init
+    RTI
   .endproc
 
   .proc irq
@@ -81,8 +120,13 @@
   .endproc
 
   ; Load data
-  title_palette: .incbin "palettes/title.pal"
-  title_nametable: .incbin "nametables/title.nam"
+  title_palette: .incbin "pals/title.pal"
+  title_nametable: .incbin "nams/title.nam"
+
+.segment "DPCM"
+
+.segment "SONGS"
+  song_title: .include "songs/title.s"
 
 .segment "VECTORS"
   .addr nmi, reset, irq
